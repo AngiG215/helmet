d05c32e07840529a1ce7a29d3633c2f9e78b7b42
Angela <angieadrigarcia572@gmail.com>
2026-02-21 17:48:07 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,23 +2,10 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-// 1. Desafío Helmet
 app.use(helmet.hidePoweredBy());
 
-// 2. Seguridad adicional (esto ayuda a que los tests pasen mejor)
-app.use(helmet.noSniff());
-app.use(helmet.xssFilter());
-
-// 3. Configuración para que server.js no falle
-app.get("/", function (request, response) {
-  response.sendFile(__current_dir + '/views/index.html');
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando");
 });
 
-app.use(express.static('public'));
-
-app.get("/_api/app-info", function(req, res) {
-  res.json({headers: res.getHeaders(), appStack: []});
-});
-
-// 4. LA EXPORTACIÓN CORRECTA
-module.exports = app;
+module.exports = app; // <--- ESTO ES LO ÚNICO AL FINAL
