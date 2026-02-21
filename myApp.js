138dae5c76978fb0fd284d2159c41e643d8a42bc
Angela <angieadrigarcia572@gmail.com>
2026-02-21 00:56:55 -0300

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+
+// --- AÑADE ESTO ---
+const helmet = require('helmet');
+app.use(helmet());
+// ------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+module.exports = app;
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
