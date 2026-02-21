7d2c1315775884682af6364938b4bb08b39f3eb4
Angela <angieadrigarcia572@gmail.com>
2026-02-21 18:07:29 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app = express();
+const app = express(); // Este es el que usará el server.js
 const helmet = require('helmet');
 
+// El desafío:
 app.use(helmet.hidePoweredBy());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// Ruta para que la web no esté vacía
+app.get("/", function (request, response) {
+  response.send("Hello World - Helmet is active");
 });
 
-module.exports = app; // <--- Así y nada más
+module.exports = app;
