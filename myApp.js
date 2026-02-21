13ca977666f4b1ab1c6570a36c550ae2b70e8605
Angela <angieadrigarcia572@gmail.com>
2026-02-21 15:51:29 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -60,9 +60,11 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+// ESTA ES LA LÍNEA MÁS IMPORTANTE
+module.exports = app; 
 
-module.exports = app;
+// --- BORRA O COMENTA TODO LO QUE SIGUE ABAJO ---
+// let port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Your app is listening on port ${port}`);
+// });
